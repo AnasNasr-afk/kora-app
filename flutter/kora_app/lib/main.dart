f5bae8628828a30f77b0fa99ae2dcbe3eb83a7f5
Anas Nasr <anas.nasr132003@gmail.com>
2025-08-03 08:34:03 +0300

    Setup is done with splash screen and auth screen

--- flutter/kora_app/lib/main.dart
+++ flutter/kora_app/lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kora_app/core/routing/app_router.dart';
+import 'package:kora_app/core/themes/color_manager.dart';
 
 void main() {
-  runApp(const KoraApp());
+  runApp( KoraApp( appRouter: AppRouter()));
 }
 
 
 class KoraApp extends StatelessWidget {
-  const KoraApp({super.key});
+  final AppRouter appRouter;
+  const KoraApp({super.key,
+    required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Kora',
-      debugShowCheckedModeBanner: false,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'Kora',
+        theme: ThemeData(
+          primaryColor: ColorManager.primaryColor,
+
+        ),
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: appRouter.onGenerateRoute,
+      ),
     );
   }
 }
